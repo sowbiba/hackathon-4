5856bdd49fece204880e475fa8e36a01245b2ac9
sowbiba <isow@profideo.com>
2017-03-03 17:03:44 +0100

    Level C

--- LevelC/Player.php
+++ LevelC/Player.php
@@ -20,9 +20,62 @@ class Player
 
     public function getSteps($partOfTheTrack, $context)
     {
-        //var_dump($context);
-        $instruction = "MMSMM";
+//        $instruction = "MMSMM";
+//
+        var_dump($partOfTheTrack);
+        if ("" === $partOfTheTrack)
+            die();
+        $hasX = false;
+        $safeMove = 0;
+        $nextInstruction = "";
+
+        for ($i=strlen($partOfTheTrack);$i>=1;$i--) {
+            if ($safeMove === 0 && 'X' !== $partOfTheTrack[$i-1]) {
+                $safeMove = $i;
+            }
+            if ('X' === $partOfTheTrack[$i-1]) {
+                $hasX = true;
+            }
+        }
+
+        if ('_' === $partOfTheTrack) {
+            return 'M';
+        }
+
+//        if (!$hasX) {
+//            var_dump("AHHH", $hasX, $partOfTheTrack);
+//           for ($i=0;$i<strlen($partOfTheTrack)-1;$i++) {
+//               $nextInstruction .= 'M';
+//           }
+//
+//            var_dump($nextInstruction);
+//
+//            return $nextInstruction;
+//        }
+
+        var_dump("SAFE MOVE = " . $safeMove);
+        for ($i=1;$i<$safeMove;$i++) {
+            $nextInstruction .= 'M';
+        }
+
+//        for ($i=strlen($nextInstruction);$i<5;$i++) {
+//            $nextInstruction .= 'S';
+//        }
+
+        $nbS = 1;
+        for ($i=strlen($nextInstruction);$i<10;$i++) {
+            if ($nbS <= 5) {
+                $nextInstruction .= 'S';
+                $nbS++;
+            }
+        }
+
+
+        var_dump("NEXT = ", $nextInstruction);
+//        if ("" === $nextInstruction)
+//            die();
+
         // @TODO
-        return $instruction;
+        return $nextInstruction;
     }
 };
